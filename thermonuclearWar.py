eb3bb43f3cc9e0dcc4b5a0ed9a428cd9eedbb0ab
Ali Rodell <alirodell@hotmail.com>
2015-08-22 10:20:45 -0400

    Cleaned it up, added comments, fixed whitespace issue.  Updated dialogue.

--- thermonuclearWar.py
+++ thermonuclearWar.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+#
+# A simple little game inspired by the 1983 film "War Games" (admittedly a much simpler version and with my own creative license on dialogue...)
+#
+# Note on the use of curses: there are simpler console interaction frameworks out there than the curses wrapper but I opted not to use them 
+# since we are are not using frameworks for this exercise.
+#
+
 import curses
 
 # First set up the screen, turn off echo of commands and print out the opener.
@@ -18,39 +25,38 @@ event = None
 # Note: the "ord" function allows you to use characters for comparisons, otherwise you have to use the ascii values directly. 
 
 while True and event != ord('q'): 
-	# Gather the keystrokes by "getch'ing"
-   	event = screen.getch() 
+    # Gather the keystrokes by "getch'ing"
+    event = screen.getch() 
    
-   	#if event == ord("q"): break 
-	if event == ord("1"): 
-		screen.clear() 
-      		screen.border(0)
-      		screen.addstr(2, 2, "Which game would you like to play?")
-      		screen.addstr(4, 4, "1 - Thermonuclear War")
-	
-		insideEvent = screen.getch()
-		if insideEvent == ord('1'):
-			screen.clear()
-			screen.border(0)
-			screen.addstr(2, 2, "Excellent, now let us begin...")
-			screen.addstr(4, 2, "This game is quite simple, do you wish to strike first or second?")
-			screen.addstr(6, 4, "1 - First")
-			screen.addstr(8, 4, "2 - Second")
-			
-			reallyInsideEvent = screen.getch()
-			if reallyInsideEvent == ord('1') or reallyInsideEvent == ord('2'):
-	                        screen.clear()
-	                        screen.border(0)
-        	                screen.addstr(2, 2, "First or second, there is no winner in Thermonuclear War.")
-                	        screen.addstr(4, 2, "Alas, we have both lost this game.  Perhaps next time we should play a nice game of chess.")
-				screen.addstr(6, 2, "Press 1 to start again or q to exit.")
+    if event == ord("1"): 
+        screen.clear() 
+        screen.border(0)
+        screen.addstr(2, 2, "Which game would you like to play?")
+        screen.addstr(4, 4, "1 - Global Thermonuclear War")
+    
+        insideEvent = screen.getch()
+        
+        if insideEvent == ord('1'):
+            screen.clear()
+            screen.border(0)
+            screen.addstr(2, 2, "Excellent, now let us begin...")
+            screen.addstr(4, 2, "This game is quite simple, do you wish to strike first or second?")
+            screen.addstr(6, 4, "1 - First")
+            screen.addstr(8, 4, "2 - Second")
+            
+            reallyInsideEvent = screen.getch()
+            
+            if reallyInsideEvent == ord('1') or reallyInsideEvent == ord('2'):
+                screen.clear()
+                screen.border(0)
+                screen.addstr(2, 2, "First or second, there is no winner in Global Thermonuclear War.")
+                screen.addstr(4, 2, "Alas, we have both lost this game.  Perhaps next time we should play a nice game of chess.")
+                screen.addstr(6, 2, "Press 1 to start again or q to exit.")
+               
+            elif reallyInsideEvent == ord('q'): break
+        elif insideEvent == ord('q'): break     
+    elif event == ord('2'): break
 
-				# Should we grab the output so that you don't go back in the menus?
 
-			elif reallyInsideEvent == ord('q'): break
-		elif insideEvent == ord('q'): break		
-	elif event == ord('2'): break
-
-
-# Exit the menu and return to the cursor
+# Exit the menu and return to the cursor -> this resets the noecho(), etc.
 curses.endwin()
